bb35719945daf2b38c89831ccf743b72ea271869
ⓀⓄⓉ <36645434+KOT0628@users.noreply.github.com>
2025-07-18 06:03:39 +0300

    Create __init__.py

---
+++ Harvester/TG-bot/data/features/__init__.py
@@ -0,0 +1,20 @@
+from .discord_bridge import (
+    set_bot,
+    send_to_discord,
+    send_photo_to_discord,
+    send_text,
+    send_photo,
+)
+
+from .gm_gn import (
+    set_bot,
+    register_handlers,
+)
+
+from .price import (
+    send_price_image,
+    use_price,
+    set_bot,
+)
+
+from .score import register_handlers
