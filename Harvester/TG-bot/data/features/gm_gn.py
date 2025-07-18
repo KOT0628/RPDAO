06c7a78e822410069ed2c871c318da2ea7d8b9d9
ⓀⓄⓉ <36645434+KOT0628@users.noreply.github.com>
2025-07-18 06:06:16 +0300

    Create gm_gn.py

---
+++ Harvester/TG-bot/data/features/gm_gn.py
@@ -0,0 +1,57 @@
+import os
+import random
+import logging
+import threading
+
+from telebot.types import Message
+
+from config.settings import CHAT_ID, GM_IMAGE_OUTPUT, GN_IMAGE_OUTPUT
+from utils.helpers import delete_command_after, safe_delete_message, get_random_background
+from utils.image import create_greeting_image
+
+# === ВНЕШНИЕ УСТАНОВКИ ===
+# == Глобальные переменные ==
+bot = None
+
+# == Создаём списки фраз ==import os
+import random
+import logging
+import threading
+
+from telebot.types import Message
+
+from config.settings import CHAT_ID, GM_IMAGE_OUTPUT, GN_IMAGE_OUTPUT
+from utils.helpers import delete_command_after, safe_delete_message, get_random_background
+from utils.image import create_greeting_image
+
+# === ВНЕШНИЕ УСТАНОВКИ ===
+# == Глобальные переменные ==
+bot = None
+
+# == Создаём списки фраз ==
+
+...
+
+# == Установка бота ==
+def set_bot(b):
+    global bot
+    bot = b
+
+# === ОТПРАВКА ИЗОБРАЖЕНИЙ GM И GN В ЧАТ ПО КОМАНДЕ ===
+# == Обработчик команды /gm ==
+
+...
+
+# == Обработчик команды /gn ==
+
+...
+
+# == Обработчик GM/GN ==
+def register_handlers(bot):
+    gm_bot(bot)
+    gn_bot(bot)
+
+__all__ = [
+    "set_bot",
+    "register_handlers"
+]
