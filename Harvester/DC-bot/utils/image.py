e19baafed8c5db3d0f675f7109553675adb26d0c
ⓀⓄⓉ <36645434+KOT0628@users.noreply.github.com>
2025-07-29 01:35:59 +0300

    Create image.py

---
+++ Harvester/DC-bot/utils/image.py
@@ -0,0 +1,13 @@
+import os
+import logging
+import datetime
+
+from PIL import Image, ImageDraw, ImageFont
+
+from config.settings import BACKGROUND_PATH, FONT_PATH, BTC_IMAGE_OUTPUT
+
+# === ГЕНЕРАЦИЯ ИЗОБРАЖЕНИЯ С ЦЕНОЙ BTC ===
+
+...
+
+__all__ = ["create_price_image"]
