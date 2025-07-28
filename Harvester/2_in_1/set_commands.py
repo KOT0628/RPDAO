173c08786a00e373a92250f239e64154f05aa2c2
ⓀⓄⓉ <36645434+KOT0628@users.noreply.github.com>
2025-07-29 01:52:40 +0300

    Create set_commands.py

---
+++ Harvester/2_in_1/set_commands.py
@@ -0,0 +1,30 @@
+import os
+import json
+
+import requests
+
+TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+
+url = f"https://api.telegram.org/bot{TOKEN}/setMyCommands"
+
+commands = [
+    {"command": "price", "description": "Show current price $BTC"},
+    {"command": "roll", "description": "ROLL"},
+    {"command": "reroll", "description": "Throw the dice"},
+    {"command": "gm", "description": "Good morning RPDAO"},
+    {"command": "gn", "description": "Good night RPDAO"},
+    {"command": "rpdao_game", "description": "Game Zone RPDAO"},
+    {"command": "crimson_board", "description": "Leaderboard Crimson Envoys"},    
+    {"command": "link", "description": "Official links Red Planet DAO"},
+    {"command": "stop", "description": "Disabling the use of slash commands"},
+    {"command": "start", "description": "Enabling the use of slash commands"},
+]
+
+response = requests.post(url, json={"commands": commands})
+
+if response.ok:
+    print("Команды успешно установлены!")
+else:
+    print("Ошибка при установке команд:")
+    print(response.text)
+    
