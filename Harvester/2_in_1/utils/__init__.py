1d1eaa923b35692765a5bec61626ef6096971913
ⓀⓄⓉ <36645434+KOT0628@users.noreply.github.com>
2025-07-29 02:38:13 +0300

    Create __init__.py

---
+++ Harvester/2_in_1/utils/__init__.py
@@ -0,0 +1,55 @@
+from .check_admin import admin_only
+
+from .dc_helpers import (
+    escape_markdown,
+    previous_price,
+    save_last_price,
+    last_tweet_id,
+    save_last_tweet_id,
+    seen_tweet_ids,
+)
+
+from .helpers import (
+    escape_md,
+    is_recent,
+    safe_delete_message,
+    delete_command_after,
+    get_random_background,
+    require_bot_active,
+)
+
+from .image import (
+    create_price_image,
+    create_greeting_image,
+)
+
+from .logger import clear_log_file
+
+from .message_map import (
+    save_mapping,
+    get_telegram_reply_id,
+)
+
+from .scheduler import run_scheduler
+
+from .scoreboard import (
+    save_scores,
+    add_point,
+    add_point_trivia,
+    user_score_pages,
+    scores,
+    points,
+    show_score_page,
+    handle_score_pagination,
+)
+
+from .translate import translate_smart
+
+from .telegram_notify import send_ticket_alert
+
+from .tweets_guard import (
+    allow_check,
+    mark_checked,
+    block_due_to_limit,
+    should_skip_fetch,
+)
