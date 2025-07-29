c889112329c35a10f3bf2f2cd138d17db3c58c81
ⓀⓄⓉ <36645434+KOT0628@users.noreply.github.com>
2025-07-29 06:16:51 +0300

    Update tweets_guard.py

--- Harvester/2_in_1/utils/tweets_guard.py
+++ Harvester/2_in_1/utils/tweets_guard.py
@@ -4,6 +4,7 @@ import logging
 
 TWITTER_TIMESTAMP_FILE = "data/twitter/last_twitter_check.txt"
 TWITTER_BLOCK_FILE = "data/twitter/twitter_block.txt"
+TWITTER_DISABLED_FILE = "data/twitter/twitter_disabled.txt"
 
 # === ПРОВЕРКА ИНТЕРВАЛА МЕЖДУ ЗАПРОСАМИ ===
 
@@ -26,6 +27,10 @@ TWITTER_BLOCK_FILE = "data/twitter/twitter_block.txt"
 ...
 
 # === ПРОПУСК ЗАПРОСА, ЕСЛИ НЕ ТРЕБУЕТСЯ ===
+
+...
+
+# === ЗАГРУЗКА СОСТОЯНИЯ БЛОКИРОВКИ TWITTER ===
 
 ...
 
