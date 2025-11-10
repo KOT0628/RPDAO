2d43bdb8bcdac063f1e10923bf5d4ea32157f5f0
ⓀⓄⓉ <kot67676@gmail.com>
2025-11-10 05:45:47 +0200

    Update handlers.py

--- Harvester/2_in_1/features/game/handlers.py
+++ Harvester/2_in_1/features/game/handlers.py
@@ -1,9 +1,11 @@
+from . import blackjack_handlers
 from . import game_menu
 from . import roll
 from . import reroll
 from . import trivia
 
 def set_bot(bot):
+    blackjack_handlers.set_bot(bot)
     game_menu.set_bot(bot)
     roll.set_bot(bot)
     reroll.set_bot(bot)
@@ -16,6 +18,9 @@ def set_save_scores(callback):
 
 # print("[DEBUG] Импорт handlers.py")
 def register_game_handlers(bot):
+    blackjack_handlers.start_blackjack(bot)
+    blackjack_handlers.start_blackjack_callbacks(bot)
+    
     roll.start_roll_handlers(bot)
     roll.use_roll(bot)
     roll.stop_roll_handlers(bot)
